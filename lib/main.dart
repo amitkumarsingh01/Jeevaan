aa9ab50256b5cd3e0afa938636b3ec210896ff9d
amitkumarsingh01 <aksmlibts@gmail.com>
2025-10-19 01:21:17 +0530

    Updating lib\main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'screens/splash_screen.dart';
 import 'services/medication_reminder_service.dart';
 import 'services/dummy_data_service.dart';
+import 'database/database_helper.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,11 +10,21 @@ void main() async {
   // Initialize medication reminder service
   await MedicationReminderService.initialize();
   
+  // Reset database to ensure all tables are created
+  final dbHelper = DatabaseHelper();
+  await dbHelper.resetDatabase();
+  
+  // Add sample users if none exist
+  await DummyDataService.addDummyUsers();
+  
   // Add dummy doctors if none exist
   await DummyDataService.addDummyDoctors();
   
   // Add dummy medicines if none exist
   await DummyDataService.addDummyMedicines();
+  
+  // Add sample orders if none exist
+  await DummyDataService.addDummyOrders();
   
   runApp(const MyApp());
 }
